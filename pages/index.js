b1286613ef3c83a1b3e40a220a657c3424607aa6
ToMattBan <souaquelecleyton@yandex.com>
2022-06-08 16:03:29 -0300

    Organizando funções

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,34 @@ import * as THREE from "three";
 export default function Home() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  const objects = {};
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  function start() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    camera.position.z = 5;
+  }
 
-  camera.position.z = 5;
+  function addCube() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    objects.cube = new THREE.Mesh(geometry, material);
+    scene.add(objects.cube);
+  }
 
-  function animate() {
-    requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
+  function animateCube() {
+    requestAnimationFrame(animateCube);
+  
+    objects.cube.rotation.x += 0.01;
+    objects.cube.rotation.y += 0.01;
+  
     renderer.render(scene, camera);
   }
 
-  animate();
+  start();
+  addCube();
+  animateCube();
 
   return (
     <div>
